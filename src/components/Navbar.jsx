47b2aad49fd024d162fe2f13af1d655184250f56
A HAYDEN ANAND <haydenanand9@gmail.com>
2026-02-08 14:51:23 +0530

    Refactor Navbar links and update background color for Testimonials and WhyChooseInkflow sections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,6 @@ function Navbar() {
 
         <div className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
           <Link to="/" className="nav-link">Home</Link>
-          <Link to="/products" className="nav-link">Products and Services</Link>
           <Link to="/about" className="nav-link">About Us</Link>
           <Link to="/products" className="nav-link">Products and Services</Link>
           <Link to="/contact" className="nav-link">Contact Us</Link>
